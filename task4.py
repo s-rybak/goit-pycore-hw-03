184637032c4b97b34ede5a8c730022074456951a
sgoithw <birch_deck_0s@icloud.com>
2025-02-21 20:53:53 +0100

    Refactored code according to notices

--- task4.py
+++ task4.py
@@ -5,20 +5,36 @@ def get_upcoming_birthdays(users):
     today = datetime.now()
     upcoming_birthdays = []
     for user in users:
-        user_birthday = datetime.strptime(user['birthday'], '%Y.%m.%d')
-        if user_birthday.month == today.month and user_birthday.day >= today.day and user_birthday.day <= today.day + 7:
-            notification_date = user_birthday.replace(year=today.year)
-            if notification_date.weekday() == 5 :
-                notification_date = notification_date + timedelta(days=2)
-            elif notification_date.weekday() == 6:
-                notification_date = notification_date + timedelta(days=1)
-
-            upcoming_birthdays.append({
-                'name': user['name'],
-                'congratulation_date': notification_date.strftime('%Y.%m.%d')
-            })
-            
+        user_birthday = get_user_birthday(user)
+        if is_birthday_upcoming(user_birthday):
+            upcoming_birthdays.append(get_user_notification_object(user, user_birthday))
     return upcoming_birthdays
+
+def is_birthday_upcoming(user_birthday):
+    today = datetime.now()
+    b_delta = (user_birthday - today).days
+    return b_delta < 7 and b_delta >= 0
+
+def get_user_birthday(user):
+    today = datetime.now()
+    b_day = datetime.strptime(user['birthday'], '%Y.%m.%d').replace(year=today.year)
+    if b_day < today:
+        return b_day.replace(year=today.year + 1)
+    return b_day
+
+def get_user_notification_object(user, user_birthday):
+    return {
+        'name': user['name'],
+        'congratulation_date':  get_notification_date(user_birthday).strftime('%Y.%m.%d')
+    }
+
+def get_notification_date(user_birthday):
+     notification_date = user_birthday
+     if notification_date.weekday() == 5 :
+          notification_date = notification_date + timedelta(days=2)
+     elif notification_date.weekday() == 6:
+        notification_date = notification_date + timedelta(days=1)
+     return notification_date
 
 users = [
     {"name": "John Doe", "birthday": "1985.02.23"},
@@ -27,6 +43,7 @@ users = [
     {"name": "Jane Smith 2", "birthday": "1990.02.28"},
     {"name": "Jane Smith 2", "birthday": "1990.03.01"},
     {"name": "Jane Smith 2", "birthday": "1990.03.22"},
+    {"name": "Jane Smith 2", "birthday": "1990.02.19"},
 ]
 
 print(get_upcoming_birthdays(users))
